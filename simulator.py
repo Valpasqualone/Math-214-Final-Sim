77e5ef398282f4b98e131541c2e35fc01e5b2e71
Valentina Pasqualone <155025063+Valpasqualone@users.noreply.github.com>
2024-04-20 12:57:20 -0400

    Add files via upload
    Added graph functionality

--- simulator.py
+++ simulator.py
@@ -15,36 +15,60 @@ JUVENILE = 1
 ADULT = 2
 #
 CURRENTYEAR = 2024
-transformer = numpy.array([[0,           0,    75/20], 
-                           [0.56, 0.94737 * 0.82, 0], 
-                           [0,   0.0526 * 0.82, 0.9]], float)
+transform_matrix = numpy.array([[0,           0,    75/7], 
+                           [0.56, (17/18) * 0.82, 0], 
+                           [0,   (1/18) * 0.82, 0.9]], float)
 #I have a thing in the google doc explaining how I got these changed numbers for the transition matrix
 #feel free to mess with the values and see how it changes the pop over time 
 start_matrix = numpy.array([0, 0, 0], float)
 #Reads the data set into data
+data = []
 with open(age_file, mode='r') as file:
     reader = csv.reader(file)
+    highestyear = 0
     for row in reader:
-        #data.append(row)
-        if(row[AGE] != "Age"):
-            totalage = int(row[AGE]) + (2024 - int(row[YEAR]))
-            if(totalage <= 1):
-                start_matrix[HATCHLING] += 1
-            elif(totalage > 1 and totalage < 20):
-                start_matrix[JUVENILE] += 1
-            else:
-                start_matrix[ADULT] += 1
+        data.append(row)
+        #print(row[YEAR])
+        if(row[YEAR] != "Year"):
+            if(int(row[YEAR]) > highestyear):
+                highestyear = int(row[YEAR])
+        
+    CURRENTYEAR = highestyear
+
+for item in data:
+    if(item[AGE] != "Age"):
+        totalage = int(item[AGE]) + (CURRENTYEAR - int(item[YEAR]))
+        if(totalage <= 1):
+            start_matrix[HATCHLING] += 1
+        elif(totalage > 1 and totalage < 20):
+            start_matrix[JUVENILE] += 1
+        else:
+            start_matrix[ADULT] += 1
 
 past_trans = [start_matrix]
+egg = [start_matrix[HATCHLING]]
+mid = [start_matrix[JUVENILE]]
+grown = [start_matrix[ADULT]]
+totalpop = [egg[0] + mid[0] + grown[0]]
+year = [CURRENTYEAR]
 for i in range(21):
-    year = CURRENTYEAR + i
 
     if(i != 0):
         #Recursively applying the transformation matrix
-        past_trans.append(numpy.matmul(transformer, past_trans[i-1]))
-    result = past_trans[i]
-    egg = int(result[HATCHLING])
-    hatch = int(result[JUVENILE])
-    grown = int(result[ADULT])
-    print("{0}: {1} hatchlings, {2} juveniles, and {3} adults.\n".format(year, egg, hatch, grown))
+        year.append( (year[i-1] + 1))
+        past_trans.append(numpy.matmul(transform_matrix, past_trans[i-1]))
+        result = past_trans[i]
+        egg.append(int(result[HATCHLING]))
+        mid.append(int(result[JUVENILE]))
+        grown.append(int(result[ADULT]))
+        totalpop.append(egg[i] + mid[i] + grown[i])
+        
+    print("{0}: {1} hatchlings, {2} juveniles, and {3} adults.\n".format(year[i], egg[i], mid[i], grown[i]))
 
+#Creating the graphs:
+fig = matplotlib.pyplot.figure(figsize = (10, 5))
+matplotlib.pyplot.bar(year, grown, color='navy', width=0.4,)
+matplotlib.pyplot.xlabel("Year")
+matplotlib.pyplot.ylabel("Adult Population")
+matplotlib.pyplot.title("Adult Population Over Time")
+matplotlib.pyplot.show()    
